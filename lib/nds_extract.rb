0395304c2ba024c6ac2951a16236374b41ccc53d
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 03:02:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -71,7 +71,9 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
   hash = {}
-  movie
+  movie_index = 0 
+  while movie_index < collection.length do 
+    
 end 
 
 def movies_with_directors_set(source)
